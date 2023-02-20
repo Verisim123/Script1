b61690cd644262ffacf1c055bcb4b48f029cf3f2
Verisim123 <120422449+Verisim123@users.noreply.github.com>
2023-02-19 22:22:34 -0500

    Add files via upload

---
+++ Script.lua
@@ -0,0 +1,9 @@
+local MyName = 'Kieran'
+if MyName == 'Kieran' then
+    print ('My name is Kieran')
+elseif MyName == 'Kiddo' then
+    print ('My name is kiddo')
+else
+    print ('My name is NOT Kieran')
+
+end
